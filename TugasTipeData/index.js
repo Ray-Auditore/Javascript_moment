942d0782ddacbdb289189aed8991dd0c91a68884
Ray-Auditore <151702591+Ray-Auditore@users.noreply.github.com>
2024-05-07 13:47:00 +0700

    update tugas tipe data
    Update tugas tipe data

--- TugasTipeData/index.js
+++ TugasTipeData/index.js
@@ -5,14 +5,25 @@ console.log(hasil_perkalian);
 var buku = "Sejarah";
 console.log(buku);
 
-var hargaBuku = 50.0;
+var hargaBuku = 50000;
 console.log(hargaBuku);
 
 var bukuYangDibeli = 10;
 console.log(bukuYangDibeli);
 
-var totalHarga = hargaBuku * bukuYangDibeli;
-console.log(totalHarga);
+var totalSejarah = hargaBuku * bukuYangDibeli
+console.log(totalSejarah);
+
+var totalharga3 =
+  "nama buku " +
+  buku +
+  " harga buku nya " +
+  hargaBuku +
+  " total terbeli " +
+  bukuYangDibeli +
+  " total Harga " +
+  totalSejarah
+  console.log(totalharga3);
 
 // cara cepet + pusing
 var buku = "aqidah";
@@ -22,33 +33,33 @@ var discount = 0.5;
 var totalDiscount = hargaBuku * discount;
 var hargaBukuSetelahDiscount = 50.0;
 var totalHarga2 = hargaBuku * bukuYangDibeli;
-console.log(
+var afterDiscount =
   "nama buku " +
-    buku +
-    " harga buku nya " +
-    hargaBuku +
-    " total terbeli " +
-    bukuYangDibeli +
-    " total Harga " +
-    totalHarga2
-);
-console.log("harga setelah discount" + totalHarga * discount);
+  buku +
+  " harga buku nya " +
+  hargaBuku +
+  " total terbeli " +
+  bukuYangDibeli +
+  " total Harga " +
+  totalHarga2;
+console.log(afterDiscount);
+console.log("harga setelah discount" + totalHarga2 * discount);
 
 // buku ke 2
 var buku1 = "ipa";
 var harga1 = 23000;
 var yangDibeli = 20;
 var total = harga1 * yangDibeli;
-console.log(
+var ad =
   "nama buku " +
-    buku1 +
-    " harga buku nya " +
-    harga1 +
-    " total terbeli " +
-    yangDibeli +
-    " total Harga " +
-    total
-);
+  buku1 +
+  " harga buku nya " +
+  harga1 +
+  " total terbeli " +
+  yangDibeli +
+  " total Harga " +
+  total;
+console.log(ad);
 // total tiga buku
-var totalTigaBuku = totalHarga + totalHarga2 + total
-console.log(totalTigaBuku);
+var totalTigaBuku = totalHarga2 + totalHarga2 + total;
+console.log(ad + "  " + afterDiscount + " " + totalharga3+" total harga seluruh nya " + totalTigaBuku);
