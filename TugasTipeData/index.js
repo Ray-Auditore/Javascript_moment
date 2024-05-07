e94e1e160abc9e145539a02039bf49085319a79e
Ray-Auditore <151702591+Ray-Auditore@users.noreply.github.com>
2024-05-07 13:15:41 +0700

    Tugas Tipe Data
    latihan penjualan

---
+++ TugasTipeData/index.js
@@ -0,0 +1,54 @@
+var hasil_perkalian = 6 * 6;
+console.log(hasil_perkalian);
+
+// tugas no 2
+var buku = "Sejarah";
+console.log(buku);
+
+var hargaBuku = 50.0;
+console.log(hargaBuku);
+
+var bukuYangDibeli = 10;
+console.log(bukuYangDibeli);
+
+var totalHarga = hargaBuku * bukuYangDibeli;
+console.log(totalHarga);
+
+// cara cepet + pusing
+var buku = "aqidah";
+var hargaBuku = 100000;
+var bukuYangDibeli = 10;
+var discount = 0.5;
+var totalDiscount = hargaBuku * discount;
+var hargaBukuSetelahDiscount = 50.0;
+var totalHarga2 = hargaBuku * bukuYangDibeli;
+console.log(
+  "nama buku " +
+    buku +
+    " harga buku nya " +
+    hargaBuku +
+    " total terbeli " +
+    bukuYangDibeli +
+    " total Harga " +
+    totalHarga2
+);
+console.log("harga setelah discount" + totalHarga * discount);
+
+// buku ke 2
+var buku1 = "ipa";
+var harga1 = 23000;
+var yangDibeli = 20;
+var total = harga1 * yangDibeli;
+console.log(
+  "nama buku " +
+    buku1 +
+    " harga buku nya " +
+    harga1 +
+    " total terbeli " +
+    yangDibeli +
+    " total Harga " +
+    total
+);
+// total tiga buku
+var totalTigaBuku = totalHarga + totalHarga2 + total
+console.log(totalTigaBuku);
