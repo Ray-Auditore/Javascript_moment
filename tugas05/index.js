1076db7866f69d93ffe4fb8edd8f5086e5d62cdd
Ray-Auditore <151702591+Ray-Auditore@users.noreply.github.com>
2024-05-14 10:47:04 +0700

    tugas 05
    tugas 05

---
+++ tugas05/index.js
@@ -0,0 +1,65 @@
+// no 1
+let permen = 30;
+let ana = 30 - 10;
+console.log(ana);
+var permenBaru = 20 * 2;
+console.log(permenBaru);
+var permenBaru = 40 ** 2;
+console.log(permenBaru);
+
+// no 2
+let kucing = "ali" + "hasan";
+console.log(kucing);
+
+// no 3 jika string ditambah depan nya '+' maka akan jadi numeric 
+let belalang = "12";
+let nyamuk = "11";
+let angsa = 10;
+let belalangNyamuk = +belalang + nyamuk
+console.log(belalangNyamuk);
+let nyamukAngsa = +nyamuk + angsa
+console.log(nyamukAngsa);
+
+// no 4
+let bukuFiksi = 5000
+let bukuNonFiksi = 7000
+let pena = 2000
+let total1 = 5
+let total2 = 3
+let total3 = 2
+console.log(bukuFiksi*total1+bukuNonFiksi*total2+pena*total3);
+
+// no 5
+let nilai1 = 5
+let nilai2= 3
+let nilai3 = 2
+console.log(nilai1 + nilai2 * nilai3);
+
+// no 6
+let kresek = 4
+let kotak = 6
+let ember = 5+(kresek=kotak)
+console.log(ember);
+
+// no 7
+let hurufX = 5 + 10 + 15
+let z = hurufX
+let y = hurufX
+console.log(hurufX);
+console.log(z);
+console.log(y);
+
+// no 8
+let nilai = 4
+let Nilai = nilai * 2
+let nilai0 = Nilai * 2
+console.log(nilai0);
+
+// no 9
+var x=5
+x++
+console.log(x);
+let nilaiY=8
+nilaiY--
+console.log(nilaiY);
+
